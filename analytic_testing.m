beb770e988655ff7c47bc73e5232c71c488b257e
Grabt234 <gnorrie12@gmail.com>
2021-06-09 16:43:02 +0200

    solving for indv pfa

--- analytic_testing.m
+++ analytic_testing.m
@@ -1,3 +1,5 @@
+%% case varibles
+
 D_lower = 0.8; 
 D_upper = 0.95;
 samples = 15;
@@ -7,8 +9,45 @@ D_step = (D_upper - D_lower)/samples;
 D = D_lower:D_step:D_upper;
 
 %converting to number of detected pulses 
-N_transmitted = 100
+N = 100;
+Nt = repmat(N,1,samples+1);
 
 %number of sucessfully detected pulses
-N_alarmed = D*N_transmitted;
+Na = D*N;
+
+PFA = 10e-4;
+
+    
+%% calcs
+%one operates with the simplification: Pfa << 1
+
+%now one needs to solve for an individual pulses pfs
+
+syms pfa 
+
+assume(pfa, 'real')
+assumeAlso(pfa>0)
+assumeAlso(pfa<0.5)
+assumptions
+
+i = 1;
+eqn = (factorial(Nt(i))/(factorial(i)*factorial(Nt(i)-Na(i))))*pfa^(Na(i))*(1-pfa)^(Nt(i)-Na(i)) == PFA
+
+
+pfa = vpasolve(eqn,pfa,[0 0.5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
