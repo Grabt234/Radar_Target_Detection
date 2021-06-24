be8e557a04d42953d65b98d54dc445af0d1e9467
Grabt234 <gnorrie12@gmail.com>
2021-06-24 16:06:40 +0200

    snr betterish

--- phased_array_toolbox.m
+++ phased_array_toolbox.m
@@ -1,4 +1,4 @@
 figure
 %rocpfa(1e-4,'NumPulses',1,'SignalType','real')
-rocpfa(1e-4,'NumPulses',1,'SignalType','NonfluctuatingCoherent')
+rocpfa(10e-3    ,'NumPulses',1,'SignalType','NonfluctuatingCoherent')
 %rocpfa(1e-8,'NumPulses',10,'SignalType','NonfluctuatingNonCoherent')
